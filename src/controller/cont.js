f6a924c9857a1c24a6456ef69ef83ba63b79afc1
Yaman Shaikh <yamansidaid@gmail.com>
2025-06-12 14:15:48 +0530

    complete

--- src/controller/cont.js
+++ src/controller/cont.js
@@ -10,14 +10,13 @@ exports.loginpage=(req,res)=>{
 }
 exports.login = async (req, res) => {
     const { username, password } = req.body;
-
     try {
         const { result, role } = await model.log(username, password);
         console.log("user name ",username);
-        console.log("passwird ",password);
+        console.log("password ",password);
         console.log("Query result ",result);
         if (result.length === 0) {
-            return res.redirect("invalid");
+            return res.redirect("/register");
         }
 
         req.session.userid = result[0].id || result[0].staff_id;
@@ -31,5 +30,5 @@ exports.login = async (req, res) => {
     }
 };
 exports.register=(req,res)=>{
-    res.send("invalid ");
+    res.send("invalid  ");
 }
