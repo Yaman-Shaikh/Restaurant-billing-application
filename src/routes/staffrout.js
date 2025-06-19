0f9f40be152c94f06e759e38b4cefd7f620bc02b
Yaman Shaikh <yamansidaid@gmail.com>
2025-06-19 17:35:01 +0530

    complete the staff dashborad and orders data

--- src/routes/staffrout.js
+++ src/routes/staffrout.js
@@ -11,7 +11,15 @@ app.get('/tables', staffController.getTablesPage);
 app.get('/menus', staffController.getMenusPage);
 // Static pages
 //app.get('/menus', (req, res) => res.render('staff/menus'));
-app.get('/orders', (req, res) => res.render('staff/orders'));
+
 app.get('/reports', (req, res) => res.render('staff/reports'));
+
+
+
+
+app.get("/staffdashboard", staffController.getStaffDashboard);
+app.post('/save-order', staffController.saveOrder);
+app.get('/orders', staffController.getAllOrders);
+app.get('/staff/orders/:id/bill', staffController.generateBill);
 
 module.exports = app;
